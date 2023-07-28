079a187cce74eac03acf9c8ebe0b1beb03c3caa2
h-r-wells2 <hrwells10246@gmail.com>
2023-07-28 19:31:45 +0530

    objects

---
+++ 02basics/myObjects.js
@@ -0,0 +1,14 @@
+"use strict";
+// const User = {
+//     name: "hrwells",
+//     email: "hrwells@lco.dev",
+//     isAvtive: true
+// }
+Object.defineProperty(exports, "__esModule", { value: true });
+var myUser = {
+    _id: "1245",
+    name: "h",
+    email: "hrwells@h.com",
+    isActive: false
+};
+myUser.email = "hrwells@gmail.com";
